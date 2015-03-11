ebf8d7ab0904502d94e7108b105b356791c07a77
moicirk <imurujev@gmail.com>
2015-03-11 20:17:06 +0200

    repo init

---
+++ src/ActiveRecord.php
@@ -0,0 +1,12 @@
+<?php
+namespace mongoex;
+
+use yii\mongodb\ActiveRecord as BaseActiveRecord;
+
+/**
+ * @author Igor Murujev <imurujev@gmail.com>
+ */
+class ActiveRecord extends BaseActiveRecord
+{
+    
+}
---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,15 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-mongodb' => 
+  array (
+    'name' => 'yiisoft/yii2-mongodb',
+    'version' => '2.0.3.0',
+    'alias' => 
+    array (
+      '@yii/mongodb' => $vendorDir . '/yiisoft/yii2-mongodb',
+    ),
+  ),
+);
